8061261dacecd29f532319cc226aaea1b7982ac4
mzjoel <mzhssn313@gmail.com>
2025-04-07 14:46:49 +0700

    feat:setup laravel filament resources

---
+++ app/Filament/Resources/SertificationResource.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\SertificationResource\Pages;
+use App\Filament\Resources\SertificationResource\RelationManagers;
+use App\Models\Sertification;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class SertificationResource extends Resource
+{
+    protected static ?string $model = Sertification::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                Tables\Filters\TrashedFilter::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                    Tables\Actions\ForceDeleteBulkAction::make(),
+                    Tables\Actions\RestoreBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListSertifications::route('/'),
+            'create' => Pages\CreateSertification::route('/create'),
+            'edit' => Pages\EditSertification::route('/{record}/edit'),
+        ];
+    }
+
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()
+            ->withoutGlobalScopes([
+                SoftDeletingScope::class,
+            ]);
+    }
+}
---
+++ app/Filament/Resources/SertificationResource/Pages/CreateSertification.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\SertificationResource\Pages;
+
+use App\Filament\Resources\SertificationResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateSertification extends CreateRecord
+{
+    protected static string $resource = SertificationResource::class;
+}

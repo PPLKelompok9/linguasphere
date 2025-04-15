f4548bc854ba2e2e65242e2ac77aaae4c15801c7
muhammadidzoha <muhammadidzoha@gmail.com>
2025-04-15 22:24:41 +0700

    (+) add design from Homepage and update design login and register

--- resources/views/components/input-label.blade.php
+++ resources/views/components/input-label.blade.php
@@ -1,5 +1,11 @@
-@props(['value'])
+@props(['value', 'icon' => null, 'for' => null])
 
-<label {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700 dark:text-gray-300']) }}>
-    {{ $value ?? $slot }}
+@if ($value)
+  <p class="font-semibold text-obito-text-primary">{{ $value }}</p>
+@endif
+<label for="{{ $for }}" class="relative group">
+  {{ $slot }}
+  @if ($icon)
+    <img src="{{ $icon }}" class="absolute size-5 flex shrink-0 transform -translate-y-1/2 top-1/2 left-5" alt="icon">
+  @endif
 </label>
